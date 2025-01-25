ed0a6f8ae3e0466b9c45cd1bea7c99df6b8cf07b
BjankaDollenga <161649276+BjankaDollenga@users.noreply.github.com>
2025-01-25 09:42:09 +0100

    Update hamburger-button.js

--- js/hamburger-button.js
+++ js/hamburger-button.js
@@ -1,1 +1,7 @@
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const body = document.body;
 
+hamburgerBtn.addEventListener('click', () => {
+  body.classList.toggle('mobile-menu-active');
+  hamburgerBtn.classList.toggle('active');
+});
